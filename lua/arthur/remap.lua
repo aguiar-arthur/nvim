b14cbcaddbec232a132367606d32fa37511d3d6a
Aguiar575 <aguiarus99arthurus@gmail.com>
2024-01-26 23:48:15 -0300

    adding support to lazygit

--- lua/arthur/remap.lua
+++ lua/arthur/remap.lua
@@ -2,8 +2,11 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- terminal mappings
-vim.keymap.set("n", "<C-/>", "<cmd>term<CR>i")
+vim.keymap.set("n", "<C-/>", "<cmd>term<CR>")
 vim.keymap.set('t', "<C-\\>", "<C-\\><C-n>:bd!<CR>",{silent = true})
+
+-- lazygit support
+vim.keymap.set("n", "<C-g>g", "<cmd>term lazygit<CR>a")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
