7b7b5dcb7d049f5ebdefda82df516b6602da846f
Arthur Aguiar <aguiarus99arthurus@gmail.com>
2024-01-31 08:15:03 -0300

    chanding nvim theme;

--- lua/arthur/packer.lua
+++ lua/arthur/packer.lua
@@ -8,18 +8,9 @@ return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.3',
-        -- or                            , branch = '0.1.x',
+        'nvim-telescope/telescope.nvim', branch = '0.1.x',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
-
-    use({
-        'rose-pine/neovim',
-        as = 'rose-pine',
-        config = function()
-            vim.cmd('colorscheme rose-pine')
-        end
-    })
 
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
     use('nvim-treesitter/playground')
@@ -28,7 +19,18 @@ return require('packer').startup(function(use)
     use('tpope/vim-fugitive')
     use('nvim-tree/nvim-web-devicons')
     use('lewis6991/gitsigns.nvim')
-    
+    use 'rmehri01/onenord.nvim'
+
+    use {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        requires = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+        }
+    }
+
     use {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v2.x',
