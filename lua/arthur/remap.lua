df417158d40553bc8499f4a650b3fb51abd3eb69
Arthur Aguiar <aguiarus99arthurus@gmail.com>
2024-02-07 00:33:22 -0300

    removing unused remaps

--- lua/arthur/remap.lua
+++ lua/arthur/remap.lua
@@ -44,9 +44,3 @@ vim.keymap.set("n", "<leader>vpp", packer);
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
-
--- move this cursor!
-vim.api.nvim_set_keymap('i', '<C-h>', '<Left>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', '<C-j>', '<Down>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', '<C-k>', '<Up>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', '<C-l>', '<Right>', { noremap = true, silent = true })
